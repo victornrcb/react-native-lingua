f6e1e250dd35b60fc0034acabb0485fff9a0a738
victornrcb <2006.naranjocabreravictor@gmail.com>
2026-06-28 18:05:25 +0100

    Implement design system

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,35 @@
 import '../../global.css';
 import { Stack } from "expo-router";
+import { 
+  useFonts, 
+  Poppins_400Regular, 
+  Poppins_500Medium, 
+  Poppins_600SemiBold, 
+  Poppins_700Bold 
+} from "@expo-google-fonts/poppins";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return <Stack />;
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,24 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex justify-center items-center">
-      <Text className="text-xl text-indigo-600 text-center mt-90">
-        Edit src/app/index.tsx to edit this screen.
+    <View className="flex-1 justify-center items-center bg-background px-4">
+      <Text className="text-h1 text-lingua-purple mb-4">
+        Lingua App
       </Text>
+      <Text className="text-body-lg text-text-primary mb-2">
+        Learn new languages quickly!
+      </Text>
+      <View className="bg-surface p-4 rounded-xl border border-border w-full">
+        <Text className="text-h3 text-lingua-deep-purple mb-2">
+          Design System Setup
+        </Text>
+        <Text className="text-body-md text-text-secondary">
+          Colors, typographies, and fonts are successfully configured.
+        </Text>
+        <View className="mt-4 bg-success px-4 py-2 rounded-lg items-center">
+          <Text className="text-body-lg text-background font-poppins-semibold">Get Started</Text>
+        </View>
+      </View>
     </View>
   );
 }
